6e14c318e17ea69ab4c7db41b8f57cc9f747ddce
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-06-17 12:57:57 +0300

    Migrate to functional component Re-write App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormAddContact from './FormAddContact';
 import { Alert, Container } from 'react-bootstrap';
 import Section from './Section';
@@ -6,58 +6,50 @@ import Contacts from './Contacts';
 import { nanoid } from 'nanoid';
 import Filter from './Filter';
 
-export default class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  componentDidMount() {
+  //Read data from localStorage on mount phase
+  useEffect(() => {
     const contacts = localStorage.getItem('contacts');
     try {
       const parsedContacts = JSON.parse(contacts);
       if (parsedContacts) {
-        this.setState({ contacts: parsedContacts });
+        setContacts(parsedContacts);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  }, []);
 
-  findContact = name => {
-    const { contacts } = this.state;
+  //Save data to localStorage after contacts update
+  useEffect(() => {
+    if (contacts.length) localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const findContact = name => {
     const normalizedName = name.toLowerCase();
     return contacts.find(({ name }) => name.toLowerCase() === normalizedName);
   };
 
-  addContact = ({ name, number }) => {
-    if (this.findContact(name)) {
+  const addContact = ({ name, number }) => {
+    if (findContact(name)) {
       alert(`${name} is already in contacts`);
       return;
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { name, number, id: nanoid() }],
-    }));
+    setContacts(prevState => [...prevState, { name, number, id: nanoid() }]);
   };
 
-  onDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const onDelete = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  onFilterChange = e => {
-    this.setState({ filter: e.target.value });
+  const onFilterChange = e => {
+    setFilter(e.target.value);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(
@@ -67,27 +59,23 @@ export default class App extends Component {
     );
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-    return (
-      <Container className="w-50 p-3">
-        <Section title="Phonebook">
-          <FormAddContact onSubmit={this.addContact} />
-        </Section>
-        <Section title="Contacts">
-          {contacts.length !== 0 ? (
-            <>
-              <Filter value={filter} onChange={this.onFilterChange} />
-              <Contacts
-                contacts={this.getFilteredContacts()}
-                onDelete={this.onDelete}
-              />
-            </>
-          ) : (
-            <Alert variant="info">Contact list is empty</Alert>
-          )}
-        </Section>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container className="w-50 p-3">
+      <Section title="Phonebook">
+        <FormAddContact onSubmit={addContact} />
+      </Section>
+      <Section title="Contacts">
+        {contacts.length !== 0 ? (
+          <>
+            <Filter value={filter} onChange={onFilterChange} />
+            <Contacts contacts={getFilteredContacts()} onDelete={onDelete} />
+          </>
+        ) : (
+          <Alert variant="info">Contact list is empty</Alert>
+        )}
+      </Section>
+    </Container>
+  );
+};
+
+export default App;
