a406a0ee41aa77347dedc88845ba25569385dd6d
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-07-23 15:55:09 +0300

    Add handling of loading state and error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 import FormAddContact from './FormAddContact';
-import { Container } from 'react-bootstrap';
+import { Alert, Container } from 'react-bootstrap';
 import Section from './Section';
 import Contacts from './Contacts';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllThunk } from 'redux/actions';
+import { selectError, selectIsLoading } from 'redux/selectors';
 
 const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  console.log(error);
+
   useEffect(() => {
     dispatch(fetchAllThunk());
   }, [dispatch]);
@@ -18,6 +23,17 @@ const App = () => {
         <FormAddContact />
       </Section>
       <Section title="Contacts">
+        {isLoading && !error && (
+          <Alert variant="secondary">Request in progress...</Alert>
+        )}
+        {error && (
+          <Alert variant="danger" onClose={() => dispatch(fetchAllThunk())} dismissible>
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+              {error}
+            </p>
+          </Alert>
+        )}
         <Contacts />
       </Section>
     </Container>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,17 +2,30 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { addContactData, deleteContactData, fetchContactsData } from "utils/api";
 
 export const fetchAllThunk = createAsyncThunk(
-  'contacts/fetchAll', async () => {
-    return await fetchContactsData();
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      return await fetchContactsData();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const addContactThunk = createAsyncThunk(
-  'contacts/addContact', async (contactData) => {
-    return await addContactData(contactData);
+  'contacts/addContact', async (contactData, thunkAPI) => {
+    try {
+      return await addContactData(contactData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 )
 
-export const deleteContactThunk = createAsyncThunk('contacts/deleteContact', async (contactId) => {
-  return await deleteContactData(contactId);
+export const deleteContactThunk = createAsyncThunk('contacts/deleteContact', async (contactId, thunkAPI) => {
+  try {
+    return await deleteContactData(contactId);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
 })
