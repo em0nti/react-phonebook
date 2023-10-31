f20a46ef7398228a7f4b9051411120ad92d26de9
em0nti <39101979+em0nti@users.noreply.github.com>
2023-10-31 16:22:27 +0200

    Update dependency, install mantine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+
+// other css files are required only if
+// you are using components from the corresponding package
+// import '@mantine/dates/styles.css';
+// import '@mantine/dropzone/styles.css';
+// import '@mantine/code-highlight/styles.css';
+// ...
 import React, { useEffect } from 'react';
 import FormAddContact from './FormAddContact';
 import { Alert, Container } from 'react-bootstrap';
@@ -9,40 +18,34 @@ import { selectError, selectIsLoading } from 'redux/selectors';
 import Filter from './Filter';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+	const dispatch = useDispatch();
+	const isLoading = useSelector(selectIsLoading);
+	const error = useSelector(selectError);
 
-  useEffect(() => {
-    dispatch(fetchAllThunk());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchAllThunk());
+	}, [dispatch]);
 
-  return (
-    <Container className="w-50 p-3">
-      <Section title="Phonebook">
-        <FormAddContact />
-      </Section>
-      <Section title="Contacts">
-        {isLoading && !error && (
-          <Alert variant="secondary">Request in progress...</Alert>
-        )}
-        {error && (
-          <Alert
-            variant="danger"
-            onClose={() => dispatch(fetchAllThunk())}
-            dismissible
-          >
-            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-            <p>{error}</p>
-          </Alert>
-        )}
-        <>
-          <Filter />
-          <Contacts />
-        </>
-      </Section>
-    </Container>
-  );
+	return (
+		<Container className="w-50 p-3">
+			<Section title="Phonebook">
+				<FormAddContact />
+			</Section>
+			<Section title="Contacts">
+				{isLoading && !error && <Alert variant="secondary">Request in progress...</Alert>}
+				{error && (
+					<Alert variant="danger" onClose={() => dispatch(fetchAllThunk())} dismissible>
+						<Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+						<p>{error}</p>
+					</Alert>
+				)}
+				<>
+					<Filter />
+					<Contacts />
+				</>
+			</Section>
+		</Container>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { MantineProvider } from '@mantine/core';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<MantineProvider theme={theme}>
+				<App />
+			</MantineProvider>
+		</Provider>
+	</React.StrictMode>
 );
